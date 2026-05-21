6b6ffd326c89655ed78f1d514491e3a705b7a026
Daniel <danieladler326@gmail.com>
2026-05-21 12:54:21 +0200

    Completado la clase Inquilino

--- src/Modelo/Inquilino.java
+++ src/Modelo/Inquilino.java
@@ -1,4 +1,99 @@
 package Modelo;
 
 public class Inquilino {
+
+    // ATRIBUTOS
+
+    private int id;
+    private String DNI;
+    private String nombre;
+    private String correo;
+    private String telefono;
+    private boolean tiene_mascota;
+
+    // SETTERS
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setTiene_mascota(boolean tiene_mascota) {
+        this.tiene_mascota = tiene_mascota;
+    }
+
+    // GETTERS
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public boolean isTiene_mascota() {
+        return tiene_mascota;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    // CONSTRUCTORES
+
+    public Inquilino(int id, String DNI, String nombre, String correo, String telefono, boolean tiene_mascota) {
+        this.id = id;
+        this.tiene_mascota = tiene_mascota;
+        if (telefono.isBlank()) {
+            this.telefono = null;
+        } else {
+            this.telefono = telefono;
+        }
+        if (correo.isBlank()) {
+            this.correo = null;
+        } else {
+            this.correo = correo;
+        }
+        this.nombre = nombre;
+        this.DNI = DNI;
+    }
+
+    // METODOS
+
+    @Override
+    public String toString() {
+        return "╔══════════════════════════════════╗\n" +
+                "║         DATOS DEL CLIENTE        ║\n" +
+                "╠══════════════════════════════════╣\n" +
+                "║ ID       : " + id + "\n" +
+                "║ DNI      : " + DNI + "\n" +
+                "║ Nombre   : " + nombre + "\n" +
+                "║ Correo   : " + correo + "\n" +
+                "║ Teléfono : " + telefono + "\n" +
+                "║ Mascota  : " + (tiene_mascota ? "Sí" : "No") + "\n" +
+                "╚══════════════════════════════════╝";    }
 }
