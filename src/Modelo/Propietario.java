d2c81ef5c27bdf7e8c720733e084473e24d5b990
Daniel <danieladler326@gmail.com>
2026-05-21 12:32:11 +0200

    Completado la clase Propietario

--- src/Modelo/Propietario.java
+++ src/Modelo/Propietario.java
@@ -1,4 +1,97 @@
 package Modelo;
 
 public class Propietario {
+
+    // ATRIBUTOS
+    private int id;
+    private String DNI;
+    private String nombre;
+    private String correo;
+    private String telefono;
+
+    // SETTERS
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    // GETTERS
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    // CONSTRUCTORES
+
+    public Propietario() {
+        this.id = -1;
+        this.DNI = null;
+        this.nombre = null;
+        this.correo = null;
+        this.telefono = null;
+    }
+
+
+    public Propietario(int id, String DNI, String nombre, String correo, String telefono) {
+        this.id = id;
+        this.DNI = DNI;
+        this.nombre = nombre;
+        if (correo.isBlank()){
+            this.correo = null;
+        }
+        this.correo = correo;
+        if (telefono.isBlank()){
+            this.telefono = null;
+        }
+        this.telefono = telefono;
+    }
+
+    // METODOS
+
+
+    @Override
+    public String toString() {
+        return """
+            ╔══════════════════════════════════╗
+            ║         DATOS DEL CLIENTE        ║
+            ╠══════════════════════════════════╣
+            ║ ID      : """ + id + "\n" +
+                "║ DNI     : " + DNI + "\n" +
+                "║ Nombre  : " + nombre + "\n" +
+                "║ Correo  : " + correo + "\n" +
+                "║ Teléfono: " + telefono + "\n" +
+                "╚══════════════════════════════════╝";
+    }
 }
