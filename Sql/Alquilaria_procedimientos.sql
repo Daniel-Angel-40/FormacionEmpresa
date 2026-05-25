9b9a72a03e4dcfdd085b205e4eeede88b9bb71af
Daniel <danieladler326@gmail.com>
2026-05-25 12:29:48 +0200

    Script de procedimientos añadido: Propietario

---
+++ Sql/Alquilaria_procedimientos.sql
@@ -0,0 +1,112 @@
+-- Procedimientos para hacer CRUD
+USE alquilaria_bd;
+
+
+-- Procedimientos para Propietario -------------------------------------------------------------------------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS sp_ins_propietario;
+DELIMITER //
+CREATE PROCEDURE sp_ins_propietario(
+	IN p_DNI VARCHAR(11),
+    IN p_nombre VARCHAR(150),
+    IN p_correo VARCHAR(200),
+    IN p_telefono VARCHAR(11),
+    OUT err INT,
+    OUT id INT)
+BEGIN
+	
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		SET err = 1;
+        SET id = -1;
+        ROLLBACK;
+    END;
+
+	START TRANSACTION;
+    
+	INSERT INTO propietario (DNI, nombre, correo_electronico, telefono) VALUES(p_DNI, p_nombre, p_correo, p_telefono);
+
+	SET id = LAST_INSERT_ID();
+
+	COMMIT;
+    SET err = 0;
+END //
+DELIMITER ;
+
+
+USE alquilaria_bd;
+
+DROP PROCEDURE IF EXISTS sp_get_propietario;
+DELIMITER //
+CREATE PROCEDURE sp_get_propietario(
+	IN p_id INT)
+BEGIN
+	SELECT * FROM propietario WHERE id = p_id;
+END //
+DELIMITER ;
+
+
+USE alquilaria_bd;
+
+DROP PROCEDURE IF EXISTS sp_del_propietario;
+DELIMITER //
+CREATE PROCEDURE sp_del_propietario(
+	IN p_id INT,
+    OUT err INT,
+    OUT filas INT)
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		SET err = 1;
+        SET filas = 0;
+        ROLLBACK;
+    END;
+
+	START TRANSACTION;
+    
+    DELETE FROM vivienda WHERE id_propietario = p_id;
+    
+    SET filas = ROW_COUNT();
+    
+    DELETE FROM propietario WHERE id = p_id;
+    
+    COMMIT;
+    SET err = 0;
+END //
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS sp_upd_propietario;
+DELIMITER //
+CREATE PROCEDURE sp_upd_propietario(
+	IN p_id INT,
+    IN p_DNI VARCHAR(11),
+    IN p_nombre VARCHAR(150),
+    IN p_correo VARCHAR(200),
+    IN p_telefono VARCHAR(11),
+    OUT err INT)
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		SET err = 1;
+        ROLLBACK;
+    END;
+    
+    START TRANSACTION;
+	
+    UPDATE propietario SET DNI = p_DNI, nombre = p_nombre, correo_electronico = p_correo, telefono = p_telefono WHERE id = p_id;
+    
+    IF ROW_COUNT() = 0 THEN
+		SET err = 1;
+        ROLLBACK;
+	ELSE
+		SET err = 0;
+		COMMIT;
+    END IF;
+END//
+DELIMITER ;
+
+
+
+-- Procedimientos para la tabla Vivienda -------------------------------------------------------------------------------------------------------------------------------------------
