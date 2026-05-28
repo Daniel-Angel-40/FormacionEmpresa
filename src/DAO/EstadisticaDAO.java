351d3521d2a9351e501a7c62a1e921b16d6185da
Daniel <danieladler326@gmail.com>
2026-05-28 11:45:33 +0200

    Clase DAO con las funciones para sacar las estadisticas

---
+++ src/DAO/EstadisticaDAO.java
@@ -0,0 +1,101 @@
+package DAO;
+
+import ConexionBD.ConexionBD;
+import Modelo.Contrato;
+import Modelo.Vivienda;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class EstadisticaDAO {
+
+    public static ArrayList<Vivienda> viviendasLibres() {
+
+        ArrayList<Vivienda> listaViviendas = new ArrayList<>();
+
+        try (Connection con = ConexionBD.getConnection()) {
+
+            String sql = "{CALL sp_get_viviendas_libres()}";
+
+            CallableStatement cs = con.prepareCall(sql);
+
+            ResultSet rs = cs.executeQuery();
+
+            while (rs.next()) {
+                Vivienda v = new Vivienda(rs.getString("id"), rs.getInt("id_propietario"),
+                        rs.getDouble("alquiler_mensual"), rs.getString("direccion"),
+                        rs.getDouble("superficie"), rs.getString("descripcion"),
+                        rs.getBoolean("permite_mascota"), rs.getString("tipo"));
+
+                listaViviendas.add(v);
+            }
+            cs.close();
+        } catch (SQLException e) {
+            System.out.println("Error SQL: " + e.getMessage());
+        }
+        return listaViviendas;
+    }
+
+    public static ArrayList<Vivienda> viviendasActivasPropietario(int idPropietario) {
+
+        ArrayList<Vivienda> listaViviendas = new ArrayList<>();
+
+        try (Connection con = ConexionBD.getConnection()) {
+
+            String sql = "{CALL sp_get_viviendas_activas(?)}";
+
+            CallableStatement cs = con.prepareCall(sql);
+
+            cs.setInt(1, idPropietario);
+
+            ResultSet rs = cs.executeQuery();
+
+            while (rs.next()) {
+                Vivienda v = new Vivienda(rs.getString("id"), rs.getInt("id_propietario"),
+                        rs.getDouble("alquiler_mensual"), rs.getString("direccion"), rs.getDouble("superficie"),
+                        rs.getString("descripcion"), rs.getBoolean("permite_mascota"), rs.getString("tipo"));
+
+                listaViviendas.add(v);
+            }
+            cs.close();
+        } catch (SQLException ex) {
+            System.out.println("Error SQL: " + ex.getMessage());
+
+        }
+        return listaViviendas;
+    }
+
+    public static ArrayList<Contrato> historicoInquilino(int idInquilino) {
+
+        ArrayList<Contrato> listaContratos = new ArrayList<>();
+
+        try(Connection con = ConexionBD.getConnection()) {
+
+            String sql = "{CALL sp_get_historico_inquilino(?)}";
+
+            CallableStatement cs = con.prepareCall(sql);
+
+            cs.setInt(1, idInquilino);
+
+            ResultSet rs = cs.executeQuery();
+
+            while (rs.next()) {
+
+                Contrato c = new Contrato(rs.getInt("id"), rs.getString("id_vivienda"), rs.getInt("id_inquilino"),
+                        rs.getDouble("precio"), rs.getString("fecha_fin"),
+                        rs.getString("fecha_inicio"), rs.getString("estado"));
+
+                listaContratos.add(c);
+            }
+
+            cs.close();
+        } catch (SQLException e) {
+            System.out.println("Error SQL: " + e.getMessage());
+        }
+
+        return listaContratos;
+    }
+}
